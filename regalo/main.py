3a99aa0000cd95a9e9c2b5a07987b339a2027e58
Basileal Imana <yosephbasileal@yahoo.com>
2015-04-03 15:43:53 -0400

    Sign up and Sign in handler added

--- regalo/main.py
+++ regalo/main.py
@@ -1,22 +1,44 @@
+#EXTERNAL LIBRARIES
 import webapp2
-
-import re       #regular expressions
-
-import jinja2   #templating library
-import os       #to get template direcotry
-
-from google.appengine.ext import db #google datastore library
-
+import re
+import jinja2
+import os 
+from google.appengine.ext import db
+import string
 import random
 import hashlib
 import hmac
 from string import letters
 
+#FOR HASHING COOKIE
 secret = "imsosecret"
+
+#INIT TEMPLATE DIRECTORY
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
+#REGULAR EXPRESSION VALIDATION----------
+FIRST_RE = re.compile(r"^[a-zA-Z]{1,20}$") #atleast one characters, atmost 20, only letters
+LAST_RE  = re.compile(r"^[a-zA-Z]{1,20}$") #atleast one characters, atmost 20, only letters
+USER_RE = re.compile(r"^[a-zA-Z0-9_]{5,20}$") #atelast five characters, atmost 20, letters, numbers, underscore, hyphen
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$') #m@m.m format
+PASS_RE = re.compile(r"^.{5,20}$") #atleast five characters, atmost 20
+
+#validate user inputs
+def valid_firstname(first_name):
+    return FIRST_RE.match(first_name)
+def valid_lastname(last_name):
+    return LAST_RE.match(last_name)
+def valid_username(username):
+    return USER_RE.match(username)
+def valid_password(password):
+    return PASS_RE.match(password)
+def valid_email(email):
+    return EMAIL_RE.match(email)
+#---------------------------------------
+
+#HANDLING COOKIES-----------------------
 def make_secure_val(val):
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
@@ -24,52 +46,24 @@ def check_secure_val(secure_val):
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
-
-class Handler(webapp2.RequestHandler):
-    def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
-    
-    def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
-
-    def render(self, template, **kw):
-        self.write(self.render_str(template, **kw))
-
-    def login(self, user):
-        self.set_secure_cookie('user_id', str(user.key().id()))
-
-    def logout(self):
-        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
-
-    def set_secure_cookie(self, name, val):
-        cookie_val = make_secure_val(val)
-        self.response.headers.add_header(
-            'Set-Cookie',
-            '%s=%s; Path=/' % (name, cookie_val))
-
-    def read_secure_cookie(self, name):
-        cookie_val = self.request.cookies.get(name)
-        return cookie_val and check_secure_val(cookie_val)
-
-class MainHandler(Handler):
-    def get(self):   
-        self.render("frontpage.html")
-
-
-def make_salt(length = 5):
-    return ''.join(random.choice(letters) for x in xrange(length))
+#---------------------------------------
+#USER AUTHENTICATION--------------------
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
 
 def make_pw_hash(name, pw, salt = None):
-    if not salt:
+    if not salt: #we only create a new salt if a new user comes in
         salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
-    return '%s,%s' % (salt, h)
+    return "%s,%s" % (h,salt)
 
-def valid_pw(name, password, h):
-    salt = h.split(',')[0]
-    return h == make_pw_hash(name, password, salt)
+def valid_pw(name,pw,h):
+    salt = h.split(',')[1]
+    return h == make_pw_hash(name,pw,salt)
+#---------------------------------------
 
+
+#-----------------------------------------------------USER DB-----------------------------------------------------#
 class User(db.Model):
     first_name = db.StringProperty(required=True)
     last_name = db.StringProperty(required=True)
@@ -99,36 +93,40 @@ class User(db.Model):
     @classmethod
     def login(cls, u_name, pw):
         user = cls.by_username(u_name)
-        if user and valid_pw(u_name, pw, u.pass_hash):
+        if user and valid_pw(u_name, pw, user.pass_hash):
             return user
+#-----------------------------------------------------------------------------------------------------------------#
 
+#---------------------------------------------------MAIN HANDLER--------------------------------------------------#
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
 
-#--------------------------------------------------SIGN UP PAGE---------------------------------------------------#
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
-#Regular expressions for name, username, password, and email
-FIRST_RE = re.compile(r"^[a-zA-Z]{1,20}$") #atleast one characters, atmost 20, only letters
-LAST_RE  = re.compile(r"^[a-zA-Z]{1,20}$") #atleast one characters, atmost 20, only letters
-USER_RE = re.compile(r"^[a-zA-Z0-9_]{5,20}$") #atelast five characters, atmost 20, letters, numbers, underscore, hyphen
-EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$') #m@m.m format
-PASS_RE = re.compile(r"^.{7,20}$") #atleast seven characters, atmost 20
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
 
-#Validate user inputs
-def valid_firstname(first_name):
-    return FIRST_RE.match(first_name)
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
 
-def valid_lastname(last_name):
-    return LAST_RE.match(last_name)
- 
-def valid_username(username):
-    return USER_RE.match(username)
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header(
+            'Set-Cookie',
+            '%s=%s; Path=/' % (name, cookie_val))
 
-def valid_password(password):
-    return PASS_RE.match(password)
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+#-----------------------------------------------------------------------------------------------------------------#
 
-def valid_email(email):
-    return EMAIL_RE.match(email)
-
-# Signup page handler
+#----------------------------------------------SIGN UP PAGE HANDLER-----------------------------------------------#
 class SignUpHandler(Handler):
     def get(self):
         self.render('signup.html')
@@ -136,7 +134,7 @@ class SignUpHandler(Handler):
     def post(self):
         have_error = False
 
-        #GET REGISTER PARAMETERS
+        #Get from input
         self.first_name = self.request.get('first_name')
         self.last_name = self.request.get('last_name')
         self.user_name = self.request.get('user_name')     
@@ -144,7 +142,7 @@ class SignUpHandler(Handler):
         self.pass_verify = self.request.get('pass_verify')
         self.email = self.request.get('email')
 
-        #CHECK FOR ERRORS
+        #Check for errors
         params = dict(first_name = self.first_name, last_name = self.last_name, user_name = self.user_name, email = self.email)
         #first name
         if not self.first_name:
@@ -163,12 +161,17 @@ class SignUpHandler(Handler):
             have_error = True
 
         #user name
+
         if not self.user_name:
             params['error_username'] = "*required"
             have_error = True
         elif not valid_username(self.user_name):
             params['error_username'] = "*minimum five characters, letters, numbers and underscore"
             have_error = True
+        else:
+            user = User.by_username(self.user_name)
+            if user:
+                params['error_username'] = "*user name already exists"
 
         #password and verify passoword
         if not self.password or not self.pass_verify:
@@ -180,7 +183,7 @@ class SignUpHandler(Handler):
                 have_error = True
         if not valid_password(self.password) or self.password != self.pass_verify:
             if not valid_password(self.password):
-                params['error_password'] = "*minimum seven characters"
+                params['error_password'] = "*minimum five characters"
                 have_error = True
             elif self.password != self.pass_verify:
                 params['error_verify'] = "*your passwords didn't match"
@@ -198,19 +201,38 @@ class SignUpHandler(Handler):
         if have_error:
             self.render('signup.html', **params)
         else:
-            user = User.by_username(self.user_name)
-            if user:
-                params['error_username'] = "*user name already exists"
-                self.render('signup.html', **params)
-            else:
-                user = User.register(self.first_name, self.last_name, self.user_name, self.password, self.email)
-                user.put()
-                self.login(user)
-                #redirect to homepage?????????????????????????
+            user = User.register(self.first_name, self.last_name, self.user_name, self.password, self.email)
+            user.put()
+            self.login(user)
+            self.redirect("/")
 #-----------------------------------------------------------------------------------------------------------------#
 
+#----------------------------------------------SIGN IN PAGE HANDLER-----------------------------------------------#
+class SignInHandler(Handler):
+    def get(self):
+        self.render('signin.html')
+
+    def post(self):
+        user_name = self.request.get('username')
+        password = self.request.get('password')
+
+        user = User.login(user_name, password)
+        if user:
+            self.login(user)
+            self.redirect("/")
+        else:
+            err = 'Invalid username or password'
+            self.render('signin.html', error_login = err)
+#-----------------------------------------------------------------------------------------------------------------#
+
+#----------------------------------------------FRONT PAGE HANDLER-------------------------------------------------#
+class FrontHandler(Handler):
+    def get(self):   
+        self.render("frontpage.html")
+#-----------------------------------------------------------------------------------------------------------------#
 
 app = webapp2.WSGIApplication([ #URL handlers
-    ('/', MainHandler),
-    ('/register', SignUpHandler)
+    ('/', FrontHandler),
+    ('/register', SignUpHandler),
+    ('/signin', SignInHandler)
     ], debug=True)
